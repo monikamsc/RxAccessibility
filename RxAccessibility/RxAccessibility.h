09ed37bafce42cf6bdd139afcc21266517c3347f
emzielonka <monika@zielonka.info.pl>
2019-10-09 16:46:55 +0200

    init

---
+++ RxAccessibility/RxAccessibility.h
@@ -0,0 +1,19 @@
+//
+//  RxAccessibility.h
+//  RxAccessibility
+//
+//  Created by Monika on 09.10.19.
+//  Copyright © 2019 Monika Mścichowska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxAccessibility.
+FOUNDATION_EXPORT double RxAccessibilityVersionNumber;
+
+//! Project version string for RxAccessibility.
+FOUNDATION_EXPORT const unsigned char RxAccessibilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxAccessibility/PublicHeader.h>
+
+
